36efd770ad800a0e02c3fce910fb375dc3c07414
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-08 10:07:07 +0100

    chore: data types

--- main.go
+++ main.go
@@ -7,12 +7,24 @@ func main() {
 	var floatNum float64 = 1.23;
 	var myString string = "Working..."
 	var myRune rune = 'a'; // single quote gives you a rune datatype
+	var myBoolean bool = true
 
+	// inferred data type
+	var myVar = "text"; // automatically a string
+
+	newVar := "testing"; // shorthand
+
+	var var1, var2 = 1,2 // init multiple variables
 
 	fmt.Println("Hello World", intNum)
 	fmt.Println("Hello World", floatNum)
 	fmt.Println("Hello World", myString)
 	fmt.Println("Hello World", myRune)
+	fmt.Println("Hello World", myBoolean)
+	fmt.Println("Hello World", myVar)
+	fmt.Println("Hello World", newVar)
+	fmt.Println("Hello World", var1)
+	fmt.Println("Hello World", var2)
 	fmt.Println("Hello World", len("Y")) // length in bytes
 	fmt.Println("Hello World", utf8.RuneCountInString("Y")) // length
 }
