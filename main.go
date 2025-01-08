104ed27b6e9ea6c1dc04be58e2861aadf147b5b6
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-08 10:03:54 +0100

    chore: data types

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ func main() {
 	var intNum int = 32;
 	var floatNum float64 = 1.23;
 	var myString string = "Working..."
-
+	var myRune rune = 'a'; // single quote gives you a rune datatype
 
 
 	fmt.Println("Hello World", intNum)
 	fmt.Println("Hello World", floatNum)
 	fmt.Println("Hello World", myString)
+	fmt.Println("Hello World", myRune)
 	fmt.Println("Hello World", len("Y")) // length in bytes
 	fmt.Println("Hello World", utf8.RuneCountInString("Y")) // length
 }
