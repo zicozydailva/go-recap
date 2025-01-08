f4f598db15b0b89428ba755e98278535b1737f83
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-08 10:01:46 +0100

    chore: data types

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 import "fmt"
+import "unicode/utf8"
 
 func main() {
-	fmt.Println("Hello World")
+	var intNum int = 32;
+	var floatNum float64 = 1.23;
+	var myString string = "Working..."
+
+
+
+	fmt.Println("Hello World", intNum)
+	fmt.Println("Hello World", floatNum)
+	fmt.Println("Hello World", myString)
+	fmt.Println("Hello World", len("Y")) // length in bytes
+	fmt.Println("Hello World", utf8.RuneCountInString("Y")) // length
 }
