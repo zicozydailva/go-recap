ffb520d7adbdb4c19d07fa972605790b6dd121f8
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-08 10:25:25 +0100

    chore: maths

--- index.go
+++ index.go
@@ -7,16 +7,17 @@ func main() {
 
 	var numerator int = 11;
 	var demonimator int = 2;
-	var result int = intDivision(numerator, demonimator);
-	fmt.Println(result);
+	var result, remainder int = intDivision(numerator, demonimator);
+	fmt.Printf("The result of the integer division is %v with remainder %v", result, remainder)
 }
 
 func printMe(printVal string) {
 	fmt.Println(printVal)
 }
 
-func intDivision(numerator int, demonimator int) int {
+func intDivision(numerator int, demonimator int) (int, int) {
 	var result int = numerator / demonimator;
+	var remainder int = numerator % demonimator;
 
-	return result;
+	return result, remainder;
 }
